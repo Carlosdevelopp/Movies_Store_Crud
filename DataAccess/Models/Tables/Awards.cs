effce3d91865c425866b61f61743967524878e9b
Carlosdevelopp <carlosdevelopp@gmail.com>
2022-12-25 14:46:27 -0600

    disable

--- DataAccess/Models/Tables/Awards.cs
+++ DataAccess/Models/Tables/Awards.cs
@@ -1,17 +1,12 @@
-﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel.DataAnnotations;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using System.ComponentModel.DataAnnotations;
 
 namespace DataAccess.Models.Tables
 {
     public class Awards
     {
         [Key]
-        public int AwardId { get; set; }    
-        public string AwardTitle { get; set;}
+        public int AwardId { get; set; }
+        public string AwardTitle { get; set; } 
 
         public virtual ICollection<Movies> Movies { get; set; }
     }
--- DataAccess/Models/Tables/Movies.cs
+++ DataAccess/Models/Tables/Movies.cs
@@ -5,13 +5,13 @@ namespace DataAccess.Models.Tables
     public class Movies
     {
         [Key]
-        public int MovieId { get; set; }    
+        public int MovieId { get; set; }
         public string Title { get; set; }
         public string Description { get; set; }
         public int RunningTime { get; set; }
-        public DateTime Release { get; set; }   
-        public int GenreId { get; set; }    
-        public int AwardId { get; set; }    
+        public DateTime Release { get; set; }
+        public int GenreId { get; set; }
+        public int AwardId { get; set; }
 
         public virtual Genres Genres { get; set; }
         public virtual Awards Awards { get; set; }
